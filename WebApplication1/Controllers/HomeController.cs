648be970e13083b142ab621227a31c0ee13bd87d
serdar karakurt <serdar.karakurt@setur.com.tr>
2024-04-15 00:46:55 +0300

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Marker> markers;
+        private int markerId;
+
+        public HomeController()
+        {
+            markers = new List<Marker>();
+            markerId = 0;
+        }
+
+        public IActionResult Index()
+        {
+            return View(markers);
+        }
+
+        [HttpPost]
+        public IActionResult SaveMarker()
+        {
+            var center = new { lat = Request.Form["lat"], lng = Request.Form["lng"] };
+            var marker = new Marker { Id = markerId++, Lat = Convert.ToDouble(center.lat), Lng = Convert.ToDouble(center.lng), Datetime = DateTime.UtcNow };
+            markers.Add(marker);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult GetMarkers()
+        {
+            return Json(markers);
+        }
+
+        public IActionResult DownloadMarkers()
+        {
+            var json = JsonConvert.SerializeObject(markers);
+            var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+            return File(bytes, "application/json", "markers.json");
+        }
+    }
+
+}
---
+++ WebApplication1/Models/Marker.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Marker
+    {
+        public int Id { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public DateTime Datetime { get; set; }
+    }
+}
